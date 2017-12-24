6f632d2f8657ee29ceb6d5bef043d7445527a1c1
Zack Scholl <zack.scholl@gmail.com>
2017-12-24 06:54:20 -0700

    Add logging

---
+++ de0gee-data/src/database/logging.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.InfoLevel)
+}
