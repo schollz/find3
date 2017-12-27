7b9f75793057c37f0a9603035d20c00536ed6b7f
Zack Scholl <zack.scholl@gmail.com>
2017-12-27 15:13:23 -0700

    Seems to work

---
+++ run.py
@@ -0,0 +1,184 @@
+#!/usr/bin/python3
+
+import json
+import csv
+from random import shuffle
+import warnings
+
+import numpy
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import make_pipeline
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn import cluster, mixture
+from sklearn.neighbors import kneighbors_graph
+
+
+class AI(object):
+
+    def __init__(self):
+        self.header = []
+        self.x = []
+        self.naming = {'from': {}, 'to': {}}
+
+    def load(self):
+        header = []
+        rows = []
+        naming_num = 0
+        with open('testing/testdb.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for i, row in enumerate(reader):
+                if i == 0:
+                    header = row
+                else:
+                    for j, val in enumerate(row):
+                        if val == '':
+                            row[j] = 0
+                            continue
+                        try:
+                            row[j] = float(val)
+                        except:
+                            if val not in self.naming['from']:
+                                self.naming['from'][val] = naming_num
+                                self.naming['to'][naming_num] = val
+                                naming_num += 1
+                            row[j] = self.naming['from'][val]
+                    rows.append(row)
+
+        # first column in row is the classification, Y
+        self.y = numpy.zeros(len(rows))
+        self.x = numpy.zeros((len(rows), len(rows[0]) - 1))
+
+        record_range = list(range(len(rows)))
+        shuffle(record_range)
+        for i in record_range:
+            self.y[i] = rows[i][0]
+            self.x[i, :] = numpy.array(rows[i][1:])
+        print(self.x[0], self.y[0])
+
+    def learn(self):
+        split_for_learning = int(0.70 * len(self.y))
+        names = [
+            "Nearest Neighbors",
+            "Linear SVM",
+            "RBF SVM",
+            "Gaussian Process",
+            "Decision Tree",
+            "Random Forest",
+            "Neural Net",
+            "AdaBoost",
+            "Naive Bayes",
+            "QDA"]
+        classifiers = [
+            KNeighborsClassifier(3),
+            SVC(kernel="linear", C=0.025),
+            SVC(gamma=2, C=1),
+            GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
+            DecisionTreeClassifier(max_depth=5),
+            RandomForestClassifier(
+                max_depth=5, n_estimators=10, max_features=1),
+            MLPClassifier(alpha=1),
+            AdaBoostClassifier(),
+            GaussianNB(),
+            QuadraticDiscriminantAnalysis()]
+        for name, clf in zip(names, classifiers):
+            try:
+                clf.fit(self.x[:split_for_learning],
+                        self.y[:split_for_learning])
+                score = clf.score(self.x[split_for_learning:], self.y[
+                                  split_for_learning:])
+                print(name, score)
+            except:
+                pass
+
+ai = AI()
+ai.load()
+# ai.learn()
+params = {'quantile': .3,
+          'eps': .3,
+          'damping': .9,
+          'preference': -200,
+          'n_neighbors': 10,
+          'n_clusters': 3}
+bandwidth = cluster.estimate_bandwidth(ai.x, quantile=params['quantile'])
+connectivity = kneighbors_graph(
+    ai.x, n_neighbors=params['n_neighbors'], include_self=False)
+# make connectivity symmetric
+connectivity = 0.5 * (connectivity + connectivity.T)
+ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True)
+two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
+ward = cluster.AgglomerativeClustering(
+    n_clusters=params['n_clusters'], linkage='ward',
+    connectivity=connectivity)
+spectral = cluster.SpectralClustering(
+    n_clusters=params['n_clusters'], eigen_solver='arpack',
+    affinity="nearest_neighbors")
+dbscan = cluster.DBSCAN(eps=params['eps'])
+affinity_propagation = cluster.AffinityPropagation(
+    damping=params['damping'], preference=params['preference'])
+average_linkage = cluster.AgglomerativeClustering(
+    linkage="average", affinity="cityblock",
+    n_clusters=params['n_clusters'], connectivity=connectivity)
+birch = cluster.Birch(n_clusters=params['n_clusters'])
+gmm = mixture.GaussianMixture(
+    n_components=params['n_clusters'], covariance_type='full')
+clustering_algorithms = (
+    ('MiniBatchKMeans', two_means),
+    ('AffinityPropagation', affinity_propagation),
+    ('MeanShift', ms),
+    ('SpectralClustering', spectral),
+    ('Ward', ward),
+    ('AgglomerativeClustering', average_linkage),
+    ('DBSCAN', dbscan),
+    ('Birch', birch),
+    ('GaussianMixture', gmm)
+)
+
+for name, algorithm in clustering_algorithms:
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            message="the number of connected components of the " +
+            "connectivity matrix is [0-9]{1,2}" +
+            " > 1. Completing it to avoid stopping the tree early.",
+            category=UserWarning)
+        warnings.filterwarnings(
+            "ignore",
+            message="Graph is not fully connected, spectral embedding" +
+            " may not work as expected.",
+            category=UserWarning)
+        try:
+            algorithm.fit(ai.x)
+        except:
+            continue
+
+    if hasattr(algorithm, 'labels_'):
+        y_pred = algorithm.labels_.astype(numpy.int)
+    else:
+        y_pred = algorithm.predict(ai.x)
+    if max(y_pred) > 3:
+        continue
+    known_groups = {}
+    for i, group in enumerate(ai.y):
+        group = int(group)
+        if group not in known_groups:
+            known_groups[group] = []
+        known_groups[group].append(i)
+    guessed_groups = {}
+    for i, group in enumerate(y_pred):
+        if group not in guessed_groups:
+            guessed_groups[group] = []
+        guessed_groups[group].append(i)
+    print(name)
+    for k in known_groups:
+        for g in guessed_groups:
+            print(
+                k, g, len(set(known_groups[k]).intersection(guessed_groups[g])))
