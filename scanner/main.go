3a835af8f47b2e6f315478063b67d4937c5953fa
Zack Scholl <zack.scholl@gmail.com>
2018-02-04 10:02:37 -0500

    Try tof (doesn't work)

--- scanner/main.go
+++ scanner/main.go
@@ -7,12 +7,42 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/de0gee/de0gee-data/src/models"
+	"github.com/montanaflynn/stats"
 )
 
 func main() {
+	t := time.Now()
+	s, _ := runCommand(60*time.Second, "l2ping", "-c", "300", "-f", "0C:3E:9F:28:22:6A")
+	milliseconds := make([]float64, 300)
+	i := 0
+	for _, line := range strings.Split(s, "\n") {
+		if !strings.Contains(line, "ms") {
+			continue
+		}
+		lineSplit := strings.Fields(line)
+		msString := strings.TrimRight(lineSplit[len(lineSplit)-1], "ms")
+		ms, err := strconv.ParseFloat(msString, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		milliseconds[i] = ms
+		i++
+	}
+	milliseconds = milliseconds[:i]
+	median, err := stats.Median(milliseconds)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(median)
+	fmt.Println(time.Since(t) / 300)
+}
+
+func main2() {
 	log.Println("Testing")
 	payload := models.SensorData{}
 	payload.Timestamp = time.Now().UnixNano() / int64(time.Millisecond)
