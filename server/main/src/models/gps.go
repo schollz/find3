2a23052d069767c055155995d7338b3961c04347
Zack Scholl <zack.scholl@gmail.com>
2018-03-06 16:03:16 -0800

    Add GPS

---
+++ server/main/src/models/gps.go
@@ -0,0 +1,11 @@
+package models
+
+// GPS is the typical data structure for storing GPS data.
+type GPS struct {
+	// Timestamp is the unique identifier, the time in milliseconds
+	Timestamp int64   `json:"t"`
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lon"`
+	Altitude  float64 `json:"alt"`
+	Mac       string  `json:"mac"`
+}
