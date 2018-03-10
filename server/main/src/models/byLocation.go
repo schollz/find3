80c38e9dc21cf5f8018e2d53efe74991fb79e477
Zack Scholl <zack.scholl@gmail.com>
2018-03-10 00:33:42 -0800

    by_location response update

---
+++ server/main/src/models/byLocation.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type ByLocationDevice struct {
+	Device      string    `json:"device"`
+	Timestamp   time.Time `json:"timestamp"`
+	Probability float64   `json:"probability"`
+	Randomized  bool      `json:"random_mac"`
+}
+
+type ByLocation struct {
+	Devices  []ByLocationDevice `json:"device"`
+	Location string             `json:"location"`
+	Total    int                `json:"total"`
+}
